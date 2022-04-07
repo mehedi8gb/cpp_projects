d4f8575bb43b473271d219d9662a70d6b789a063
mehedi8gb <mehidy.gb@gmail.com>
2022-04-08 04:44:00 +0600

    improved

--- figlet.cpp
+++ figlet.cpp
@@ -1,106 +1,127 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <unistd.h>
 #include <string>
 
 using namespace std;
-void gotoxy(short x, short y)
+class Figlet
 {
-    COORD pos = {x, y};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
-void figlet(char *text, int n)
-{
-    system("cls");
-
-    for (int i = 0; i < n; i++)
+    void gotoxy(short x, short y)
     {
-        int b = 8;
-        if (text[i] == 'i')
-        {
-            b *= i;
-            gotoxy(b, 0);
-            cout << "  [][][]\n";
-            gotoxy(b, 1);
-            cout << "    []  \n";
-            gotoxy(b, 2);
-            cout << "    []  \n";
-            gotoxy(b, 3);
-            cout << "    []  \n";
-            gotoxy(b, 4);
-            cout << "  [][][]";
-        }
-        if (text[i] == 'f')
-        {
-            b *= i;
-            gotoxy(b, 0);
-            cout << "  [][][]\n";
-            gotoxy(b, 1);
-            cout << "  []    \n";
-            gotoxy(b, 2);
-            cout << "  [][]  \n";
-            gotoxy(b, 3);
-            cout << "  []    \n";
-            gotoxy(b, 4);
-            cout << "  []    ";
-        }
-        if (text[i] == 'l')
-        {
+        COORD pos = {x, y};
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    }
 
-            b *= i;
-            gotoxy(b, 0);
-            cout << "  []  \n";
-            gotoxy(b, 1);
-            cout << "  []  \n";
-            gotoxy(b, 2);
-            cout << "  []  \n";
-            gotoxy(b, 3);
-            cout << "  []  \n";
-            gotoxy(b, 4);
-            cout << "  [][][][]";
-        }
-        if (text[i] == 'e')
-        {
+public:
+    int n;
+    bool isActive = true;
+    void yn()
+    {
+        cout << "\n\n\nwant to see this magic again? y/n";
+        char c = getche();
 
-            b *= i;
-            gotoxy(b, 0);
-            cout << "  [][][][]\n";
-            gotoxy(b, 1);
-            cout << "  []      \n";
-            gotoxy(b, 2);
-            cout << "  [][][][]\n";
-            gotoxy(b, 3);
-            cout << "  []      \n";
-            gotoxy(b, 4);
-            cout << "  [][][][]";
-        }
-        if (text[i] == 'a')
-        {
+            if (c == 'y' || c == 'Y')
+                isActive = true;
+            else
+                isActive = false,cout <<"\n\tthanks for using! :D", sleep(2),exit(0);
+    }
+    void figlet(char *text, int n)
+    {
+        system("cls");
 
-            b *= i;
-            gotoxy(b, 0);
-            cout << "   []   \n";
-            gotoxy(b, 1);
-            cout << "  [][]  \n";
-            gotoxy(b, 2);
-            cout << " []  [] \n";
-            gotoxy(b, 3);
-            cout << "[][][][]\n";
-            gotoxy(b, 4);
-            cout << "[]    []";
+        for (int i = 0; i < n; i++)
+        {
+            int b = 8;
+            if (text[i] == 'i')
+            {
+                b *= i;
+                gotoxy(b, 0);
+                cout << "  [][][]\n";
+                gotoxy(b, 1);
+                cout << "    []  \n";
+                gotoxy(b, 2);
+                cout << "    []  \n";
+                gotoxy(b, 3);
+                cout << "    []  \n";
+                gotoxy(b, 4);
+                cout << "  [][][]";
+            }
+            if (text[i] == 'f')
+            {
+                b *= i;
+                gotoxy(b, 0);
+                cout << "  [][][]\n";
+                gotoxy(b, 1);
+                cout << "  []    \n";
+                gotoxy(b, 2);
+                cout << "  [][]  \n";
+                gotoxy(b, 3);
+                cout << "  []    \n";
+                gotoxy(b, 4);
+                cout << "  []    ";
+            }
+            if (text[i] == 'l')
+            {
+
+                b *= i;
+                gotoxy(b, 0);
+                cout << "  []  \n";
+                gotoxy(b, 1);
+                cout << "  []  \n";
+                gotoxy(b, 2);
+                cout << "  []  \n";
+                gotoxy(b, 3);
+                cout << "  []  \n";
+                gotoxy(b, 4);
+                cout << "  [][][][]";
+            }
+            if (text[i] == 'e')
+            {
+
+                b *= i;
+                gotoxy(b, 0);
+                cout << "  [][][][]\n";
+                gotoxy(b, 1);
+                cout << "  []      \n";
+                gotoxy(b, 2);
+                cout << "  [][][][]\n";
+                gotoxy(b, 3);
+                cout << "  []      \n";
+                gotoxy(b, 4);
+                cout << "  [][][][]";
+            }
+            if (text[i] == 'a')
+            {
+
+                b *= i;
+                gotoxy(b, 0);
+                cout << "   []   \n";
+                gotoxy(b, 1);
+                cout << "  [][]  \n";
+                gotoxy(b, 2);
+                cout << " []  [] \n";
+                gotoxy(b, 3);
+                cout << "[][][][]\n";
+                gotoxy(b, 4);
+                cout << "[]    []";
+            }
         }
     }
-}
+};
 int main()
 {
-    int n;
-    cout << "how many alphabets do you want to figlet? \n:";
-    n = getche() - '0';
-    char texts[n];
-    cout << "\nEnter Alphabets within (" << n << ") charecters \t [note: now only work with 4 char's like a,f,i,l,e]\n:";
-    cin >> texts;
-    figlet(texts, n);
+    Figlet chr;
+    while (chr.isActive)
+    {
+        cout << "\nhow many alphabets do you want to figlet? \n:";
+        chr.n = getche() - '0';
+        char texts[chr.n];
+        cout << "\nEnter Alphabets within (" << chr.n << ") charecters \t [note: now only work with 4 char's like a,f,i,l,e]\n:";
+        cin >> texts;
+        chr.figlet(texts, chr.n);
+        chr.yn();
+    }
 
     return 0;
 }
