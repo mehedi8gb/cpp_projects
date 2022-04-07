9527bac6cef366f1ca14f2f20a29f8020398f048
mehedi8gb <mehidy.gb@gmail.com>
2022-04-08 04:14:55 +0600

    improved

--- figlet.cpp
+++ figlet.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <windows.h> 
-#include <string> 
+#include <windows.h>
+#include <conio.h>
+#include <string>
 
 using namespace std;
 void gotoxy(short x, short y)
@@ -9,94 +10,97 @@ void gotoxy(short x, short y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void figlet(char *text,int n){
-system("cls");
+void figlet(char *text, int n)
+{
+    system("cls");
 
-	for (int i = 0; i < n; i++)
-	{	int b = 8;
+    for (int i = 0; i < n; i++)
+    {
+        int b = 8;
         if (text[i] == 'i')
-        {   b*=i;
-            gotoxy(b,0);
-		    cout << "  [][][]\n";
-            gotoxy(b,1);
+        {
+            b *= i;
+            gotoxy(b, 0);
+            cout << "  [][][]\n";
+            gotoxy(b, 1);
             cout << "    []  \n";
-            gotoxy(b,2);
+            gotoxy(b, 2);
             cout << "    []  \n";
-            gotoxy(b,3);
+            gotoxy(b, 3);
             cout << "    []  \n";
-            gotoxy(b,4);
+            gotoxy(b, 4);
             cout << "  [][][]";
         }
-		if (text[i] == 'f')
-        { b*=i;
-            gotoxy(b,0);
-	        cout << "  [][][]\n";
-            gotoxy(b,1);
+        if (text[i] == 'f')
+        {
+            b *= i;
+            gotoxy(b, 0);
+            cout << "  [][][]\n";
+            gotoxy(b, 1);
             cout << "  []    \n";
-            gotoxy(b,2);
+            gotoxy(b, 2);
             cout << "  [][]  \n";
-            gotoxy(b,3);
+            gotoxy(b, 3);
             cout << "  []    \n";
-            gotoxy(b,4);
+            gotoxy(b, 4);
             cout << "  []    ";
         }
-		if (text[i] == 'l')
-        {   
-            
-            b *=i;
-            gotoxy(b,0);
-	        cout << "  []  \n";
-            gotoxy(b,1);
+        if (text[i] == 'l')
+        {
+
+            b *= i;
+            gotoxy(b, 0);
             cout << "  []  \n";
-            gotoxy(b,2);
+            gotoxy(b, 1);
             cout << "  []  \n";
-            gotoxy(b,3);
+            gotoxy(b, 2);
             cout << "  []  \n";
-            gotoxy(b,4);
+            gotoxy(b, 3);
+            cout << "  []  \n";
+            gotoxy(b, 4);
             cout << "  [][][][]";
         }
-		if (text[i] == 'e')
-        {   
-            
-            b *=i;
-            gotoxy(b,0);
-	        cout << "  [][][][]\n";
-            gotoxy(b,1);
-            cout << "  []      \n";
-            gotoxy(b,2);
+        if (text[i] == 'e')
+        {
+
+            b *= i;
+            gotoxy(b, 0);
             cout << "  [][][][]\n";
-            gotoxy(b,3);
+            gotoxy(b, 1);
             cout << "  []      \n";
-            gotoxy(b,4);
+            gotoxy(b, 2);
+            cout << "  [][][][]\n";
+            gotoxy(b, 3);
+            cout << "  []      \n";
+            gotoxy(b, 4);
             cout << "  [][][][]";
         }
-		if (text[i] == 'a')
-        {   
-            
-            b *=i;
-            gotoxy(b,0);
-	        cout << "   []   \n";
-            gotoxy(b,1);
+        if (text[i] == 'a')
+        {
+
+            b *= i;
+            gotoxy(b, 0);
+            cout << "   []   \n";
+            gotoxy(b, 1);
             cout << "  [][]  \n";
-            gotoxy(b,2);
+            gotoxy(b, 2);
             cout << " []  [] \n";
-            gotoxy(b,3);
+            gotoxy(b, 3);
             cout << "[][][][]\n";
-            gotoxy(b,4);
+            gotoxy(b, 4);
             cout << "[]    []";
         }
-
-	
-	}
+    }
 }
-int main() {
+int main()
+{
     int n;
     cout << "how many alphabets do you want to figlet? \n:";
-    cin >> n;
+    n = getche() - '0';
     char texts[n];
-    cout << "Enter Alphabets within (" << n << ") charecters \t [note: now only work with 4 char's like a,f,i,l,e]\n:";
+    cout << "\nEnter Alphabets within (" << n << ") charecters \t [note: now only work with 4 char's like a,f,i,l,e]\n:";
     cin >> texts;
-    figlet(texts,n);
-    
+    figlet(texts, n);
+
     return 0;
 }
