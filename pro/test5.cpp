01ac97f09332157bd1dc4c5c2277e8bf0a8ec82c
mehedi8gb <mehidy.gb@gmail.com>
2022-04-12 23:52:29 +0600

    new tasks

---
+++ pro/test5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class rAngular
+{
+    float length;
+    float bredth;
+
+public:
+    rAngular()
+    {
+
+        cout << "Enter length" << endl;
+        cin >> length;
+        cout << "Enter bredth " << endl;
+        cin >> bredth;
+    }
+
+    ~rAngular()
+    {
+        float area = length * bredth;
+
+        cout << "Area =" << area << endl;
+    }
+};
+int main()
+{
+    rAngular o;
+}
